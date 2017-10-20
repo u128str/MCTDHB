7439f5b7519dcfb6ccaa4a9b8957c2016749e378
Alexej I. Streltsov <u128str@gmail.com>
2017-10-20 12:13:55 +0200

    Create Ubuntu_gnu.mk

---
+++ make_systems/Ubuntu_gnu.mk
@@ -0,0 +1,13 @@
+#Do not forget to install:  fftw openmpi gfortran blas lapack 
+cmp=gnu
+
+BLAS_LAPACK=gnu
+
+FFT=FFTW
+
+mpi_f90=mpif90.openmpi  -O2 -fopenmp -static
+mpi_f90=mpif90.openmpi  -O2 -fopenmp 
+#mpi_f90=mpif90.openmpi  -g -fopenmp  -fcheck=bounds
+
+FFTW_INC_DIR=/usr/include/
+FFTW_LIB_DIR=/usr/lib
